6dada5bf092ec5fc446346f811edd8f4099376e7
sasasa <masaakisaeki@gmail.com>
2020-02-20 10:07:43 +0900

    グループ化して表示する処理の途中。 まだうまくいかない。コレクションの操作でつまづいている。

--- app/Http/Controllers/SkillUserController.php
+++ app/Http/Controllers/SkillUserController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Skill;
 use DB;
 use Illuminate\Support\Facades\Auth;
+use Arr;
 
 class SkillUserController extends Controller
 {
@@ -14,9 +15,26 @@ class SkillUserController extends Controller
         $user = Auth::user();
         // DB::enableQueryLog();
         // dd(DB::getQueryLog());
+        
+        // dd(Skill::all()->groupBy(function($skill){
+        //     return $skill->type;
+        // }));
+        // dd($ret);
+
+        // $ret = [];
+        // dd(
+        //     Skill::all()->groupBy('type')->map(function($skill) use($ret){
+        //         return $ret[$skill->type][strval($skill->id)] = $skill->name;
+        //     })
+        // );
+
         return view('skilluser.create', [
             'skills' => Skill::all(),
-            'userskillids' => $user->skillIds()
+            'skillsGroupByType' => Skill::all()->groupBy('type'),
+            'skillsGroupByTypeForSelect' => Skill::all()->mapToGroups(function($skill) {
+                return [$skill->type => [$skill->id => $skill->name]];
+            }),
+            'userskillids' => $user->skillIds(),
         ]);
     }
 
--- resources/views/skilluser/create.blade.php
+++ resources/views/skilluser/create.blade.php
@@ -9,14 +9,27 @@
       <h1>skills設定</h1>
       <form method="POST" action="/skilluser">
         @csrf
-        <div class="form-check">
+        {{-- <div class="form-check">
           @foreach ($skills as $skill)
             <label class="mr-5 mb-3 form-check-label" for="skill_{{$skill->id}}">
               <input name="skills[]" class="form-check-input" type="checkbox" value="{{$skill->id}}" id="skill_{{$skill->id}}" {{in_array($skill->id, $userskillids) ? "checked" : null}}>
               {{$skill->name}}({{$skill->type}})
             </label>
           @endforeach
-        </div>
+        </div> --}}
+        
+        @foreach ($skillsGroupByType as $type => $skillsGroup)
+          <div class="form-check">
+          <legend class="small">{{$type}}</legend>
+          @foreach ($skillsGroup as $skill)
+            <label class="mr-5 mb-3 form-check-label" for="skill_{{$skill->id}}">
+              <input name="skills[]" class="form-check-input" type="checkbox" value="{{$skill->id}}" id="skill_{{$skill->id}}" {{in_array($skill->id, $userskillids) ? "checked" : null}}>
+              {{$skill->name}}
+            </label>
+          @endforeach
+          </div>
+        @endforeach
+
         <div class="form-group d-flex justify-content-end">
           <input type="submit" value="送信" class="btn btn-primary btn-lg" >
         </div>
@@ -27,9 +40,21 @@
         <div class="form-group row">
           <label class="col-sm-2 col-form-label" for="skills">スキル</label>
           <div class="col-sm-10">
-          {{
+          {{-- {{
             Form::select('skills[]',
             Arr::pluck($skills, 'name', 'id'),
+            $userskillids,
+            [
+              'class' => "form-control",
+              'id' => "skills",
+              'multiple' => true,
+              'size' => $skills->count()
+            ])
+          }} --}}
+
+          {{
+            Form::select('skills[]',
+            $skillsGroupByTypeForSelect,
             $userskillids,
             [
               'class' => "form-control",
