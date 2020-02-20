9e59bf700470acff90d03c3e36982962a0a3572d
sasasa <masaakisaeki@gmail.com>
2020-02-20 10:09:41 +0900

    グループ分けしてないものも一応表示するように変更

--- resources/views/skilluser/create.blade.php
+++ resources/views/skilluser/create.blade.php
@@ -40,7 +40,7 @@
         <div class="form-group row">
           <label class="col-sm-2 col-form-label" for="skills">スキル</label>
           <div class="col-sm-10">
-          {{-- {{
+          {{
             Form::select('skills[]',
             Arr::pluck($skills, 'name', 'id'),
             $userskillids,
@@ -50,7 +50,7 @@
               'multiple' => true,
               'size' => $skills->count()
             ])
-          }} --}}
+          }}
 
           {{
             Form::select('skills[]',
