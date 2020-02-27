d9575d7879e4812b46b248098c2f5c01ce319a0f
sasasa <masaakisaeki@gmail.com>
2020-02-27 22:55:19 +0900

    HTMLメールの際にCSSが適用されるように変更

--- resources/views/emails/sample_notification.blade.php
+++ resources/views/emails/sample_notification.blade.php
@@ -1,7 +1,32 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-  <link rel="stylesheet" href="{{{asset('/css/mail.css')}}}">
+  <style>
+body {
+  background-color: #fffacd;
+}
+h1 {
+  font-size: 16px;
+  color: #ff6666;
+}
+#button {
+  width: 200px;
+  text-align: center;
+}
+#button a {
+  padding: 10px 20px;
+  display: block;
+  border: 1px solid #2a88bd;
+  background-color: #FFFFFF;
+  color: #2a88bd;
+  text-decoration: none;
+  box-shadow: 2px 2px 3px #f5deb3;
+}
+#button a:hover {
+  background-color: #2a88bd;
+  color: #FFFFFF;
+}
+  </style>
 </head>
 <body>
 <h1>
